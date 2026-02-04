bf4ce81785dc88453507db5c3f2c658790954e64
XXN <jaechan27.0.2.28@gmail.com>
2026-02-04 20:15:14 +0900

    42. 스프링 MVC - 컨트롤러 통합

---
+++ src/main/java/com/ian/springmvc/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,41 @@
+package com.ian.springmvc.servlet.web.springmvc.v2;
+
+import com.ian.springmvc.servlet.domain.member.Member;
+import com.ian.springmvc.servlet.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        return new ModelAndView("save", "member", member);
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+
+        return new ModelAndView("members", "members", members);
+    }
+}
